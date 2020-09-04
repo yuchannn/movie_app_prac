14965b7709c4196f08ee164d5dbab088b1e442d0
yuchannn <yuchanbpp@gmail.com>
2020-09-05 02:05:11 +0900

    Updates

---
+++ precache-manifest.82d6477f53d2860d4c3d7fc3d4511c6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcba36fd2c65a25d20a8",
+    "url": "/github.io/moive_app_prac/static/css/main.acaf001b.chunk.css"
+  },
+  {
+    "revision": "fcba36fd2c65a25d20a8",
+    "url": "/github.io/moive_app_prac/static/js/main.1d757d25.chunk.js"
+  },
+  {
+    "revision": "9532617e58994262cdbb",
+    "url": "/github.io/moive_app_prac/static/js/runtime~main.cba7333a.js"
+  },
+  {
+    "revision": "d5cc5b87e6d6a7dac363",
+    "url": "/github.io/moive_app_prac/static/js/2.ff66ff15.chunk.js"
+  },
+  {
+    "revision": "6e1894aee4b1fc4c90ffe868b3166f22",
+    "url": "/github.io/moive_app_prac/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/github.io/moive_app_prac/precache-manifest.82d6477f53d2860d4c3d7fc3d4511c6a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/github.io/moive_app_prac/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
